a178a7a71ab6e39aab7a4ebd6f482a1f4290573d
JL-Huawei_Graznov <graznov@me.com>
2024-11-22 13:42:13 +0300

    2 comm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const {ObjectId} = require("mongodb");
 
 const PORT = 3000;
 
@@ -18,18 +19,59 @@ connectToDb((err)=>{
     }
 })
 
+const handleError = (res, error) => {
+    res.status(500).json({ error })
+}
+
 
 app.get('/lists', (req, res) => {
     const lists = []
-
     db
         .collection('lists')
         .find()
+        .sort({ name: 1 })
         .forEach((list)=>{lists.push(list)})
         .then(()=>{
             res
                 .status(200)
                 .json(lists)
         })
-    
+        .catch(()=> handleError(res, 'Something went wrong.'))
+
 })
+
+app.get('/lists/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)){
+        db
+            .collection('lists')
+            .findOne({ _id: new ObjectId(req.params.id) })
+            .then((doc)=>{
+                res
+                    .status(200)
+                    .json(doc)
+            })
+            .catch(()=> handleError(res, 'Something went wrong.'))
+
+    } else {
+        handleError(res, 'Wrong id')
+    }
+})
+
+app.delete('/lists/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)){
+        db
+            .collection('lists')
+            .deleteOne({ _id: new ObjectId(req.params.id) })
+            .then((result)=>{
+                res
+                    .status(200)
+                    .json(result)
+            })
+            .catch(()=> handleError(res, 'Something went wrong.'))
+
+    } else {
+        handleError(res, 'Del.Wrong id')
+    }
+})
+
+
